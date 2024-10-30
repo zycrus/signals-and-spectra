7ddee5cac740746c11be73e8b51a20ce22cfc872
Zycrus <47321670+zycrus@users.noreply.github.com>
2024-10-30 15:31:16 +0800

    Update activity-01.m

--- activity-01/activity-01.m
+++ activity-01/activity-01.m
@@ -3,23 +3,25 @@
 A = 8;
 p = 0;
 
-f = A * 1000;
-t = 1:1/f:5/f;
-f_t = A * sin(2 * pi * f * t + p);
+f1 = A * 1000;
+t1 = 1:1/f1:5/f1;
+f1_t = A * sin(2 * pi * f1 * t1 + p);
 hold on
-plot(t, f);
+plot(t1, f1);
 hold off
 
-f = f + 2;
-t = 1:1/f:5/f;
-f_t = A * sin(2 * pi * f * t + p);
+f2 = f1 + 2;
+t2 = 1:1/f3:5/f3;
+f2_t = A * sin(2 * pi * f2 * t2 + p);
 hold on
-plot(t, f);
+plot(t2, f2);
 hold off
 
-f = f - 2;
-t = 1:1/f:5/f;
-f_t = A * sin(2 * pi * f * t + p);
+f3 = f1 - 2;
+t3 = 1:1/f3:5/f3;
+f_t = A * sin(2 * pi * f3 * t3 + p);
 hold on
-plot(t, f);
+plot(t3, f3);
 hold off
+
+legend("f1" "f2" "f3");
